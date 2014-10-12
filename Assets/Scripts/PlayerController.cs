5ec042f7b88efb7b0ca3c8e4b7aac6115104d8cb
jss367 <julius.simonelli@gmail.com>
2014-10-12 13:15:51 -0400

    Asus - warning
    The game seems to have gotten much harder. Only tried to add touch in
    the gamecontroller but now there seem to be a lot more asteroids

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,32 @@ public class PlayerController : MonoBehaviour {
 		}
 	void FixedUpdate ()
 	{
+		Vector3? touchPos = null;
+		//Return whether the given mouse button is held down.
+		//button values are 0 for left button, 1 for right button, 2 for the middle button.
+		if (Input.mousePresent && Input.GetMouseButton (0))
+		{
+				touchPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+			}
+
+		else if (Input.touchCount > 0)
+		{
+			touchPos = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, 0.0f);
+		}
+
+		/*
+		if (touchPos != null)
+		{
+			target = Camera.main.ScreenToWorldPoint(touchPos.Value);
+			target.y = rigidbody.position.y;
+		}
+
+	 */
+
+		// Vector3 offest = target - rigidbody.position;
+
+
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
