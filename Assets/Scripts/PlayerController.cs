2c19832a6c53e08c9b290e74c9983640b359ff53
jss367 <julius.simonelli@gmail.com>
2014-10-12 22:35:53 -0400

    Asus
    got touch working, added a touch script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
+
+//[System.Serializable]
 public class Boundary
 {
 	public float xMin, xMax, zMin, zMax;
@@ -19,6 +20,10 @@ public class PlayerController : MonoBehaviour {
 
 	private float nextFire;
 
+	public Vector3 target;
+	public float velocityLag = 1.0f;
+	public float dampingRadius = 1.0f;
+
 	void Update(){
 
 		if (Input.GetButton("Fire1") && Time.time > nextFire) 
@@ -31,37 +36,43 @@ public class PlayerController : MonoBehaviour {
 		}
 	void FixedUpdate ()
 	{
+
+
+		//Find whether the mouse button 0 was pressed or released this frame
+//		bool b0Down = Input.GetMouseButtonDown (0);
+
 		Vector3? touchPos = null;
 		//Return whether the given mouse button is held down.
 		//button values are 0 for left button, 1 for right button, 2 for the middle button.
-		if (Input.mousePresent && Input.GetMouseButton (0))
+		if (Input.mousePresent && Input.GetMouseButton (0)) 
 		{
-				touchPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
-			}
-
-		else if (Input.touchCount > 0)
+					touchPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+			} 
+		else if (Input.touchCount > 0)  //should i add an if to see if the phase is stationary?
 		{
-			touchPos = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, 0.0f);
-		}
+					touchPos = new Vector3 (Input.touches [0].position.x, Input.touches [0].position.y, 0.0f);
+				}
 
-		/*
+			
 		if (touchPos != null)
 		{
 			target = Camera.main.ScreenToWorldPoint(touchPos.Value);
 			target.y = rigidbody.position.y;
 		}
+	 
+		Vector3 offset = target - rigidbody.position;
 
-	 */
+		float magnitude = offset.magnitude;
+			if(magnitude > dampingRadius)
+		{
+					magnitude = dampingRadius;
+		}
+		float dampening = magnitude / dampingRadius;
 
-		// Vector3 offest = target - rigidbody.position;
+		Vector3 desiredVelocity = offset.normalized * speed * dampening;
 
+		rigidbody.velocity += (desiredVelocity - rigidbody.velocity) * velocityLag;
 
-
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity = movement * speed;
 
 		rigidbody.position = new Vector3
 		(
